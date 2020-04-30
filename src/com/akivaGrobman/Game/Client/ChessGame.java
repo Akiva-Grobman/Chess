ad88ba56503ab0c53b5144feae2a16b173c931e7
akivav <akiva.grobman@gmail.com>
2020-04-30 17:19:12 +0300

    refactored move method in ChessGame

--- src/com/akivaGrobman/Game/Client/ChessGame.java
+++ src/com/akivaGrobman/Game/Client/ChessGame.java
@@ -50,40 +50,29 @@ public class ChessGame {
         }
     }
 
+    public void tileClicked(Point tilePosition) {
+        if(isLocalPlayer(currentPlayer)) {
+            player.addPositionToMove(tilePosition);
+        }
+    }
+
     public void move(Positions positions, Player player) {
-        System.out.println(player.getClass().getSimpleName() + " is making a move " + positions);
-        if(player.equals(enemy) || isLegalMove(positions)) {
-            System.out.println("is legal");
-            addMove(positions);
-            Piece piece = getPiece(positions.getOrigin());
-            assert piece != null; // if piece is null then the move shouldn't be legal
-            piece.move(positions.getDestination());
-            backendBoard.updateTile(positions.getOrigin(), null);
-            backendBoard.updateTile(positions.getDestination(), piece);
-            onScreenBoard.updateTile(positions.getDestination(), piece.getPieceType(), piece.getPieceColor());
-            onScreenBoard.updateTile(positions.getOrigin(), null, null);
-            if(wasEnpassant(backendBoard, moves)) {
-                System.out.println("enpassant");
-                backendBoard.updateTile(new Point(positions.getDestination().x, positions.getOrigin().y), null);
-                onScreenBoard.updateTile(new Point(positions.getDestination().x, positions.getOrigin().y), null, null);
-            } // todo
-             else if(wasCastling(backendBoard, moves)) {
-                //done
-            } else if(wasPromotion(backendBoard, positions.getDestination())) {
-                //done
-            }
-            if(player.equals(this.player)) {
+        if(isLocalPlayer(player) || isLegalMove(positions)) {
+            addMoveToMoveList(positions);
+            updateBoards(positions);
+            handleSpecialMoves(positions);
+            if(isLocalPlayer(player)) {
                 enemy.sendMove(positions);
             }
             changeCurrentPlayer();
-            if(player.equals(this.player)) {
+            if(isLocalPlayer(player)) {
                 Thread sendMessage = new Thread(this::makeEnemyMove);
                 sendMessage.start();
             }
         }
     }
 
-    private void addMove(Positions positions) {
+    private void addMoveToMoveList(Positions positions) {
         Piece piece;
         try {
             piece = backendBoard.getPiece(positions.getDestination());
@@ -91,6 +80,28 @@ public class ChessGame {
             piece = null;
         }
         moves.add(new Move(positions, piece));
+    }
+
+    private void updateBoards(Positions positions) {
+        Piece piece = getPiece(positions.getOrigin());
+        assert piece != null; // if piece is null then the move shouldn't be legal
+        piece.move(positions.getDestination());
+        backendBoard.updateTile(positions.getOrigin(), null);
+        backendBoard.updateTile(positions.getDestination(), piece);
+        onScreenBoard.updateTile(positions.getDestination(), piece.getPieceType(), piece.getPieceColor());
+        onScreenBoard.updateTile(positions.getOrigin(), null, null);
+    }
+
+    private void handleSpecialMoves(Positions positions) {
+        if(wasEnpassant(backendBoard, moves)) {
+            System.out.println("enpassant");
+            backendBoard.updateTile(new Point(positions.getDestination().x, positions.getOrigin().y), null);
+            onScreenBoard.updateTile(new Point(positions.getDestination().x, positions.getOrigin().y), null, null);
+        } else if(wasCastling(backendBoard, moves)) {
+            System.out.println("castling");
+        } else if(wasPromotion(backendBoard, positions.getDestination())) {
+            System.out.println("promotion");
+        }
     }
 
     public void makeEnemyMove() {
@@ -124,13 +135,13 @@ public class ChessGame {
         }
     }
 
-    public void tileClicked(Point tilePosition) {
-        if(currentPlayer.equals(player)) {
-            player.addPositionToMove(tilePosition);
-        }
+    private boolean isLocalPlayer(Player player) {
+        return this.player.equals(player);
     }
 
     public PieceColor getPlayersColor() {
         return player.getPlayersColor();
     }
+
 }
+
