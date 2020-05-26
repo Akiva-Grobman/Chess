08f58b292b7bbfd4ef6251e16b1c5eccf9bdbbcc
akivav <akiva.grobman@gmail.com>
2020-05-26 18:31:33 +0300

    ai is getting smarter

--- src/com/akivaGrobman/Game/Main.java
+++ src/com/akivaGrobman/Game/Main.java
@@ -1,6 +1,7 @@
 package com.akivaGrobman.Game;
 
 import com.akivaGrobman.Game.Client.Client;
+import com.akivaGrobman.Game.Server.Server;
 
 import java.io.IOException;
 
@@ -8,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         // todo opening window to chose multiplayer of single
-        boolean isMultiPlayerGame = false;
+        boolean isMultiPlayerGame = true;
         if(isMultiPlayerGame) {
             Client.startMultiplayerGame();
         } else {
